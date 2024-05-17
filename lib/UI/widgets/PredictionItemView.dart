dfcd7d32baf4b8bf848d8aea4212c074cda43630
NonymousMorlock <agamspaul@gmail.com>
2024-05-17 02:06:48 +0000

    refactor: Update PredictionItemView to handle unknown descriptions in the title

--- lib/UI/widgets/PredictionItemView.dart
+++ lib/UI/widgets/PredictionItemView.dart
@@ -1,19 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_webservice/places.dart';
 
 class PredictionItemView extends StatelessWidget {
   final Prediction prediction;
 
-  const PredictionItemView({Key key, this.prediction}) : super(key: key);
+  const PredictionItemView({super.key, required this.prediction});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(
-          prediction.description,
-        ),
+        title: Text(prediction.description ?? "Unknown"),
         trailing: Icon(Icons.arrow_forward),
       ),
     );
